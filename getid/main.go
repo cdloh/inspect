ae2ff5792fbdc47fd9f4316c99b0a0041d4b45d8
Dieter Plaetinck <dieter@raintank.io>
2016-07-13 18:19:35 +0100

    import from metrictank 76597b50aa9ccdd20900e9c93651dbf355f478a9

---
+++ getid/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strconv"
+
+	"github.com/davecgh/go-spew/spew"
+	"github.com/raintank/schema"
+)
+
+func usage() {
+	fmt.Fprintln(os.Stderr, "usage: getid <orgid> <name> [tag1, [tag2, ...]] # tag can be a word or key:val")
+}
+
+func main() {
+	if len(os.Args) >= 2 && (os.Args[1] == "-h" || os.Args[1] == "--help" || os.Args[1] == "help") {
+		usage()
+		os.Exit(0)
+	}
+	if len(os.Args) < 3 {
+		usage()
+		os.Exit(2)
+	}
+	orgId, err := strconv.Atoi(os.Args[1])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err.Error())
+		usage()
+		os.Exit(2)
+	}
+	if os.Args[2] == "" {
+		usage()
+		os.Exit(2)
+	}
+	md := &schema.MetricDefinition{
+		OrgId: orgId,
+		Name:  os.Args[2],
+		Tags:  os.Args[3:],
+	}
+	md.SetId()
+	spew.Dump(md)
+}
